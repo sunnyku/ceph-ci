9814e67341eed71ddf5ca449821d27d84e37f401
Sage Weil <sage@redhat.com>
2017-12-20 06:55:43 -0600

    osd: queue null events without PG lock
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -1744,9 +1744,6 @@ protected:
   TrivialEvent(Load)
   TrivialEvent(GotInfo)
   TrivialEvent(NeedUpThru)
-  public:
-  TrivialEvent(NullEvt)
-  protected:
   TrivialEvent(Backfilled)
   TrivialEvent(LocalBackfillReserved)
   TrivialEvent(RejectRemoteReservation)
