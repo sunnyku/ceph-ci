4dfe315d20fd265b806d23f7719c6607ab6c7753
Sage Weil <sage@redhat.com>
2018-01-11 09:21:54 -0600

    common/options: is_safe() -> can_update_at_runtime()
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -200,7 +200,7 @@ void Option::dump(Formatter *f) const
   dump_value("min", min, f);
   dump_value("max", max, f);
 
-  f->dump_bool("can_update_at_runtime", is_safe());
+  f->dump_bool("can_update_at_runtime", can_update_at_runtime());
 }
 
 ostream& operator<<(ostream& out, const Option::value_t& v)
@@ -255,7 +255,8 @@ void Option::print(ostream *out) const
     *out << "  Minimum: " << stringify(min) << "\n"
 	 << "  Maximum: " << stringify(max) << "\n";
   }
-  *out << "  Can update at runtime: " << (is_safe() ? "true" : "false") << "\n";
+  *out << "  Can update at runtime: "
+       << (can_update_at_runtime() ? "true" : "false") << "\n";
   if (!services.empty()) {
     *out << "  Services: " << services << "\n";
   }
