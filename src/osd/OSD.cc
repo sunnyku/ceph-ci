77efd1a73ed6090a1f11b4c515862c04c3950865
Sage Weil <sage@redhat.com>
2018-02-08 13:45:37 -0600

    osd: pass sdata into dequeue_peering_evt (and dequeue_delete)
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -8945,6 +8945,7 @@ void OSD::dequeue_op(
 
 
 void OSD::dequeue_peering_evt(
+  OSDShard *sdata,
   PG *pg,
   PGPeeringEventRef evt,
   ThreadPool::TPHandle& handle)
@@ -8984,11 +8985,13 @@ void OSD::dequeue_peering_evt(
 }
 
 void OSD::dequeue_delete(
+  OSDShard *sdata,
   PG *pg,
   epoch_t e,
   ThreadPool::TPHandle& handle)
 {
   dequeue_peering_evt(
+    sdata,
     pg,
     PGPeeringEventRef(
       std::make_shared<PGPeeringEvent>(
