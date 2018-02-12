2412446ca2eb5cf149e1a371fc838b4fbaf15f61
Venky Shankar <vshankar@redhat.com>
2018-02-11 22:29:12 -0500

    test: fixup rbd tests as a side effect of reference counting
    Allocate heap objects for Journal, ExclusiveLock and ObjectMap
    as these classes are not reference counted. However mock objects
    for these classes can be allocated on the stack as ->get() and
    ->put() operations are no-op.
    
    Signed-off-by: Mahati Chamarthy <mahati.chamarthy@intel.com>
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    (cherry picked from commit f6b4d3d0f318a21f3e0975b8d061502cfd2fda67)

--- src/test/librbd/test_internal.cc
+++ src/test/librbd/test_internal.cc
@@ -656,11 +656,14 @@ TEST_F(TestInternal, SnapshotCopyup)
         state = OBJECT_EXISTS_CLEAN;
       }
 
-      librbd::ObjectMap<> object_map(*ictx2, ictx2->snap_id);
+      librbd::ObjectMap<> *object_map = new librbd::ObjectMap<>(*ictx2, ictx2->snap_id);
       C_SaferCond ctx;
-      object_map.open(&ctx);
+      object_map->open(&ctx);
       ASSERT_EQ(0, ctx.wait());
-      ASSERT_EQ(state, object_map[0]);
+
+      std::shared_lock image_locker{ictx2->image_lock};
+      ASSERT_EQ(state, (*object_map)[0]);
+      object_map->put();
     }
   }
 }
@@ -742,13 +745,14 @@ TEST_F(TestInternal, SnapshotCopyupZeros)
         state = OBJECT_NONEXISTENT;
       }
 
-      librbd::ObjectMap<> object_map(*ictx2, ictx2->snap_id);
+      librbd::ObjectMap<> *object_map = new librbd::ObjectMap<>(*ictx2, ictx2->snap_id);
       C_SaferCond ctx;
-      object_map.open(&ctx);
+      object_map->open(&ctx);
       ASSERT_EQ(0, ctx.wait());
 
       std::shared_lock image_locker{ictx2->image_lock};
-      ASSERT_EQ(state, object_map[0]);
+      ASSERT_EQ(state, (*object_map)[0]);
+      object_map->put();
     }
   }
 }
@@ -829,13 +833,14 @@ TEST_F(TestInternal, SnapshotCopyupZerosMigration)
         state = OBJECT_NONEXISTENT;
       }
 
-      librbd::ObjectMap<> object_map(*ictx2, ictx2->snap_id);
+      librbd::ObjectMap<> *object_map = new librbd::ObjectMap<>(*ictx2, ictx2->snap_id);
       C_SaferCond ctx;
-      object_map.open(&ctx);
+      object_map->open(&ctx);
       ASSERT_EQ(0, ctx.wait());
 
       std::shared_lock image_locker{ictx2->image_lock};
-      ASSERT_EQ(state, object_map[0]);
+      ASSERT_EQ(state, (*object_map)[0]);
+      object_map->put();
     }
   }
 }
