2938df4a0c638a5ab46047da5ae3e5adddf124ea
Sage Weil <sage@redhat.com>
2018-04-06 10:26:52 -0500

    osd: implement pg merge
    - Vevamps the split tracking infrastructure, and adds new tracking for
    upcoming merges in consume_map.  These are now unified into the same
    identify_ method.  these consume the new pg_num change tracking
    instructure we just added in the prior commit.
    - PGs that are about to merge have a new wait infrastructure, since all
    sources and the target have to reach the target epoch before the merge
    can happen.
    - If one of the sources for a merge does not exist, we create an empty
    dummy PG to merge with.  The implies that the resulting merged PG will
    be incomplete (and mostly useless), but it unifies the code paths.
    - The actual merge (PG::merge_from) happens in advance_pg().
    
    Fixes: http://tracker.ceph.com/issues/85
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2859,6 +2859,10 @@ public:
     bool dirty_epoch,
     bool try_fast_info,
     PerfCounters *logger = nullptr);
+
+  void write_if_dirty(RecoveryCtx *rctx) {
+    write_if_dirty(*rctx->transaction);
+  }
 protected:
   void write_if_dirty(ObjectStore::Transaction& t);
 
