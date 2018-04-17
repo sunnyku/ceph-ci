d5d25fdba478efeb489e856fc70a5ed5f63a0620
Sage Weil <sage@redhat.com>
2018-04-16 20:40:45 -0500

    clear dirty by writing before shutdown/merge

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7919,6 +7919,8 @@ bool OSD::advance_pg(
 	  PGRef spg = pg; // carry a ref
 	  dout(1) << __func__ << " we are merge source, target is " << parent
 		   << dendl;
+	  pg->write_if_dirty(rctx);
+	  dispatch_context_transaction(rctx);
 	  pg->shutdown();  // this will osr->flush
 	  OSDShard *sdata = pg->osd_shard;
 	  {
