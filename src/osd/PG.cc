36605bdf055acb284f33d10a5344b5ed1c2d0515
Sage Weil <sage@redhat.com>
2018-05-31 08:41:02 -0500

    reset recovery pointers

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -2622,6 +2622,7 @@ void PG::merge_from(map<spg_t,PGRef>& sources, RecoveryCtx *rctx,
   pg_log.roll_forward(&handler);
 
   info.last_complete = info.last_update;  // to fake out trim()
+  pg_log.reset_recovery_pointers();
   pg_log.trim(info.last_update, info);
 
   vector<PGLog*> log_from;
@@ -2643,6 +2644,7 @@ void PG::merge_from(map<spg_t,PGRef>& sources, RecoveryCtx *rctx,
     PGLogEntryHandler handler{source.get(), rctx->transaction};
     source->pg_log.roll_forward(&handler);
     source->info.last_complete = source->info.last_update;  // to fake out trim()
+    source->pg_log.reset_recovery_pointers();
     source->pg_log.trim(source->info.last_update, source->info);
     log_from.push_back(&source->pg_log);
 
