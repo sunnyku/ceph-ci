4eb50c09e3aef02080db30d1e53fb7a46fe62fad
Venky Shankar <vshankar@redhat.com>
2018-07-03 08:06:39 -0400

    mds: allow request load average decay rate configurable via admin socket
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    (cherry picked from commit 3809d96a5b53721b88602d662c4c1cd00dd727a0)
    
    Conflicts:
    	src/mds/MDSRank.h
    	src/mds/SessionMap.h
    	src/mds/SessionMap.cc
    (cherry picked from commit 0648d461e1be9b9f50b316c8d0b4aa0c809b64f2)
    Resolves: rhbz#1618523

--- src/mds/MDSDaemon.cc
+++ src/mds/MDSDaemon.cc
@@ -378,6 +378,7 @@ const char** MDSDaemon::get_tracked_conf_keys() const
     "mds_inject_migrator_message_loss",
     "host",
     "fsid",
+    "mds_request_load_average_decay_rate",
     NULL
   };
   return KEYS;
