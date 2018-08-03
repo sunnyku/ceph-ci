ceph/ceph

mgr/orchestrator_cli: module for calling into orchestators
This is going to be pretty useful for development: calls
into the same places that the dashboard would call into.

Signed-off-by: John Spray <john.spray@redhat.com>

src/pybind/mgr/orchestrator_cli/__init__.py
@@ -1,0 +1,2 @@
+
+from orchestrator_cli import OrchestratorCli

sunnyku/ceph-ci

src/pybind/mgr/orchestrator_cli/__init__.py
@@ -1,0 +1,2 @@
+
+from module import OrchestratorCli