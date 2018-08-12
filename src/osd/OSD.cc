1fa815f4477f5f669ce5a181b831c37dc68a8902
Sage Weil <sage@redhat.com>
2018-08-12 15:04:00 -0500

    osd: remove auid session member
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6622,7 +6622,6 @@ bool OSD::ms_verify_authorizer(
   AuthCapsInfo caps_info;
   EntityName name;
   uint64_t global_id;
-  uint64_t auid = CEPH_AUTH_UID_DEFAULT;
 
   auto keys = monc->rotating_secrets.get();
   if (keys) {
@@ -6648,7 +6647,6 @@ bool OSD::ms_verify_authorizer(
     s->entity_name = name;
     if (caps_info.allow_all)
       s->caps.set_allow_all();
-    s->auid = auid;
 
     if (caps_info.caps.length() > 0) {
       auto p = caps_info.caps.cbegin();
