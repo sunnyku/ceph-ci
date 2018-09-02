ceph/ceph

mds: don't queue straydir for split
straydir can not get splitted (for now at least)

Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/mds/MDBalancer.cc
@@ -1134,7 +1134,9 @@ void MDBalancer::maybe_fragment(CDir *dir, bool hot)
 {
   // split/merge
   if (bal_fragment_dirs && bal_fragment_interval > 0 &&
-      dir->is_auth() && !dir->inode->is_base()) {  // not root/base (for now at least)
+      dir->is_auth() &&
+      !dir->inode->is_base() &&  // not root/mdsdir (for now at least)
+      !dir->inode->is_stray()) { // not straydir
 
     // split
     if (g_conf()->mds_bal_split_size > 0 && (dir->should_split() || hot)) {

sunnyku/ceph-ci

(cherry picked from commit 959e63c1f88b6ce6b2492cfb98e5875d928558e4)
Conflicts:
	src/mds/MDBalancer.cc
- finesse split/merge conditional expression

src/mds/MDBalancer.cc
@@ -1115,8 +1115,9 @@ void MDBalancer::maybe_fragment(CDir *dir, bool hot)
 {
   // split/merge
   if (g_conf->mds_bal_frag && g_conf->mds_bal_fragment_interval > 0 &&
-      !dir->inode->is_base() &&        // not root/base (for now at least)
-      dir->is_auth()) {
+      dir->is_auth() &&
+      !dir->inode->is_base() &&  // not root/base (for now at least)
+      !dir->inode->is_stray()) { // not straydir
 
     // split
     if (g_conf->mds_bal_split_size > 0 &&