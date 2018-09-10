9b273a2e71e245619f01e6adb3b67746354fbce7
Sage Weil <sage@redhat.com>
2018-09-10 14:32:03 -0500

    osd: handle null pg slot on startup
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2743,6 +2743,9 @@ int OSD::init()
   for (auto& shard : shards) {
     for (auto& i : shard->pg_slots) {
       PGRef pg = i.second->pg;
+      if (!pg) {
+	continue;
+      }
 
       pg->lock();
       set<pair<spg_t,epoch_t>> new_children;
