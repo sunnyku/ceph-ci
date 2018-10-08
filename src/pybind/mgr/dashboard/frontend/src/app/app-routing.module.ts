Devp00l/ceph

mgr/dashboard: add create functionality to the frontend
Add the missing functionality to create config options and related
tests to the frontend.

Signed-off-by: Tatjana Dehler <tdehler@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/app-routing.module.ts
@@ -76,7 +76,7 @@ const routes: Routes = [
   },
   {
     path: 'configuration',
-    data: { breadcrumbs: 'Cluster/Configuration Documentation' },
+    data: { breadcrumbs: 'Cluster/Configurations' },
     children: [
       { path: '', component: ConfigurationComponent },
       {

sunnyku/ceph-ci

src/pybind/mgr/dashboard/frontend/src/app/app-routing.module.ts
@@ -78,7 +78,7 @@ const routes: Routes = [
   },
   {
     path: 'configuration',
-    data: { breadcrumbs: 'Cluster/Configuration Documentation' },
+    data: { breadcrumbs: 'Cluster/Configuration' },
     children: [
       { path: '', component: ConfigurationComponent },
       {