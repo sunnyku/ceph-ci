83e52ffc019edcf3ed65f7aed0ec77ebb7ba4264
luo.runbing <luo.runbing@zte.com.cn>
2018-12-17 19:16:20 +0800

    mgr/dashboard: hide server info from http response
    Signed-off-by: luo.runbing <luo.runbing@zte.com.cn>

--- src/pybind/mgr/dashboard/module.py
+++ src/pybind/mgr/dashboard/module.py
@@ -107,7 +107,9 @@ class StandbyModule(MgrStandbyModule):
             'server.socket_host': server_addr,
             'server.socket_port': int(server_port),
             'engine.autoreload.on': False,
-            'log.screen': False
+            'log.screen': False,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [("Server", "ce")]
         })
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -890,7 +892,7 @@ class Module(MgrModule):
 
             @cherrypy.expose
             def index(self):
-                return self.health()
+                pass
 
             @cherrypy.expose
             @cherrypy.tools.json_out()
@@ -981,7 +983,9 @@ class Module(MgrModule):
             'server.socket_host': server_addr,
             'server.socket_port': int(server_port),
             'engine.autoreload.on': False,
-            'log.screen': False
+            'log.screen': False,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [("Server", "ce")]
         })
 
         osdmap = self.get_osdmap()
