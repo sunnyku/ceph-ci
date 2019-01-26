6cfbbd60f37eed8a3e123b2c3deaa07f5c4d80e4
Matt Benjamin <mbenjamin@redhat.com>
2019-01-26 17:43:51 -0500

    rgw: lifecycle: alternate solution to prefix_map conflict
    A rule containing a tag filter but no constraining prefix is
    legal, as is a sequence of >1 such rules.
    
    As currently designed, enforcement requires rules to be
    installed in prefix_map as well as rule_map, though only the
    latter is encoded.
    
    Fixes: http://tracker.ceph.com/issues/37879
    Resolves: rhbz#1654588
    
    Also absorbs:
    commit 2d68e7db41a89437f00314687ab1b4b81029fb7f
    Author: Yehuda Sadeh <yehuda@redhat.com>
    Date:   Fri Jul 27 16:22:12 2018 -0700
    
        but zap init_simple_days_rule
    
    https://tracker.ceph.com/issues/38055
    Resolves: rhbz#1666910
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 94b7ed4cddf28349e359dac1835ab1ef7c89ccf2,
     restaged as descending from 050cef8042a3659305a48bde72bb4d896974d95a)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -87,6 +87,14 @@ void rgw_get_errno_s3(rgw_http_error *e , int err_no)
   }
 }
 
+static inline std::string get_s3_expiration_header(
+  struct req_state* s,
+  const ceph::real_time& mtime)
+{
+  return rgwlc_s3_expiration_header(
+    s->cct, s->object, s->tagset, mtime, s->bucket_attrs);
+}
+
 struct response_attr_param {
   const char *param;
   const char *http_attr;
@@ -179,6 +187,8 @@ int RGWGetObj_ObjStore_S3::send_response_data(bufferlist& bl, off_t bl_ofs,
   map<string, string>::iterator riter;
   bufferlist metadata_bl;
 
+  string expires = get_s3_expiration_header(s, lastmod);
+
   if (sent_header)
     goto send_data;
 
@@ -249,10 +259,8 @@ int RGWGetObj_ObjStore_S3::send_response_data(bufferlist& bl, off_t bl_ofs,
 
   dump_content_length(s, total_len);
   dump_last_modified(s, lastmod);
-  if (!version_id.empty()) {
-    dump_header(s, "x-amz-version-id", version_id);
-  }
-  
+  dump_header_if_nonempty(s, "x-amz-version-id", version_id);
+  dump_header_if_nonempty(s, "x-amz-expiration", expires);
 
   if (! op_ret) {
     if (! lo_etag.empty()) {
@@ -1394,14 +1402,21 @@ void RGWPutObj_ObjStore_S3::send_response()
 	s->cct->_conf->rgw_s3_success_create_obj_status);
       set_req_state_err(s, op_ret);
     }
+
+    string expires = get_s3_expiration_header(s, mtime);
+
     if (copy_source.empty()) {
       dump_errno(s);
       dump_etag(s, etag);
       dump_content_length(s, 0);
+      dump_header_if_nonempty(s, "x-amz-version-id", version_id);
+      dump_header_if_nonempty(s, "x-amz-expiration", expires);
       for (auto &it : crypt_http_responses)
         dump_header(s, it.first, it.second);
     } else {
       dump_errno(s);
+      dump_header_if_nonempty(s, "x-amz-version-id", version_id);
+      dump_header_if_nonempty(s, "x-amz-expiration", expires);
       end_header(s, this, "application/xml");
       dump_start(s);
       struct tm tmp;
