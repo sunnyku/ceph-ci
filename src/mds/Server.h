athanatos/ceph

mds: extend reconnect period when mds is busy
Fixes: https://tracker.ceph.com/issues/37644
Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/mds/Server.h
@@ -97,6 +97,7 @@ private:
   bool reconnect_evicting;  // true if I am waiting for evictions to complete
                             // before proceeding to reconnect_gather_finish
   time reconnect_start = clock::zero();
+  time reconnect_last_seen = clock::zero();
   set<client_t> client_reconnect_gather;  // clients i need a reconnect msg from.
 
   feature_bitset_t supported_features;

sunnyku/ceph-ci

(cherry picked from commit ad1f964395f65c5d3baed712c0949928998e47be)
 Conflicts:
	src/mds/Server.cc
(cherry picked from commit 222f17d15f6bcfa6199dc8b982f3a8614d2d433e)
Resolves: rhbz#1656969

src/mds/Server.h
@@ -90,6 +90,7 @@ private:
   bool reconnect_evicting;  // true if I am waiting for evictions to complete
                             // before proceeding to reconnect_gather_finish
   time reconnect_start = time::min();
+  time reconnect_last_seen = time::min();
   set<client_t> client_reconnect_gather;  // clients i need a reconnect msg from.
 
   double cap_revoke_eviction_timeout = 0;