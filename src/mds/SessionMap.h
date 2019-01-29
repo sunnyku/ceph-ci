ceph/ceph

mds: renew client session when receiving any message
Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/mds/SessionMap.h
@@ -250,6 +250,7 @@ public:
   xlist<Capability*> caps;     // inodes with caps; front=most recently used
   xlist<ClientLease*> leases;  // metadata leases to clients
   time last_cap_renew = clock::zero();
+  time last_seen = clock::zero();
 
 public:
   version_t inc_push_seq() { return ++cap_push_seq; }

sunnyku/ceph-ci

(cherry picked from commit 593dfee07334668a912abd9732a5d138eb27058a)
 Conflicts:
	src/mds/MDSRank.cc
	src/mds/Server.cc
	src/mds/SessionMap.h
(cherry picked from commit 5dd000eb44e39567657a346451eec6adba3efb63)
Resolves: rhbz#1656969

src/mds/SessionMap.h
@@ -230,6 +230,7 @@ public:
   xlist<Capability*> caps;     // inodes with caps; front=most recently used
   xlist<ClientLease*> leases;  // metadata leases to clients
   time last_cap_renew = time::min();
+  time last_seen = time::min();
 
 public:
   version_t inc_push_seq() { return ++cap_push_seq; }