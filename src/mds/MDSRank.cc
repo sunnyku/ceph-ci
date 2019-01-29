ceph/ceph

mds: use uniform write handler for PQ
MDS would go damaged after blacklisting the MDS caused the PQ to see a write
error. Instead, just respawn and/or do usual write error handling (default
r/o).

Fixes: https://tracker.ceph.com/issues/37394

Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/MDSRank.cc
@@ -459,9 +459,10 @@ MDSRank::MDSRank(
           // Purge Queue operates inside mds_lock when we're calling into
           // it, and outside when in background, so must handle both cases.
           if (mds_lock.is_locked_by_me()) {
-            damaged();
+            handle_write_error(r);
           } else {
-            damaged_unlocked();
+            std::lock_guard l(mds_lock);
+            handle_write_error(r);
           }
         }
       )

sunnyku/ceph-ci

(cherry picked from commit 2fb665194f61914711454c2084eb1539bd3588b5)
Conflicts:
    src/msd/MDSRank.cc
- use Mutex::Locker instead of std::lock_guard
(cherry picked from commit 321597e941fc4d1b5200a3ea3226a0f928349ff4)
Resolves: rhbz#1652464

src/mds/MDSRank.cc
@@ -458,9 +458,10 @@ MDSRank::MDSRank(
           // Purge Queue operates inside mds_lock when we're calling into
           // it, and outside when in background, so must handle both cases.
           if (mds_lock.is_locked_by_me()) {
-            damaged();
+            handle_write_error(r);
           } else {
-            damaged_unlocked();
+            Mutex::Locker l(mds_lock);
+            handle_write_error(r);
           }
         }
       )