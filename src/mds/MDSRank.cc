ceph/ceph

mds: log evicted clients to clog/dbg
Fixes: http://tracker.ceph.com/issues/37639
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/MDSRank.cc
@@ -3192,11 +3192,21 @@ bool MDSRank::evict_client(int64_t session_id,
     return false;
   }
 
+  auto& addr = session->info.inst.addr;
+  {
+    CachedStackStringStream _ss;
+    auto& ss = _ss.get_stream();
+    ss << "Evicting " << (blacklist ? "(and blacklisting) " : "")
+       << "client session " << session_id << " (" << addr << ")";
+    dout(1) << ss.strv() << dendl;
+    clog->info() << ss.strv();
+  }
+
   dout(4) << "Preparing blacklist command... (wait=" << wait << ")" << dendl;
   stringstream ss;
   ss << "{\"prefix\":\"osd blacklist\", \"blacklistop\":\"add\",";
   ss << "\"addr\":\"";
-  ss << session->info.inst.addr;
+  ss << addr;
   ss << "\"}";
   std::string tmp = ss.str();
   std::vector<std::string> cmd = {tmp};

sunnyku/ceph-ci

(cherry picked from commit 7a81421453f76c6a67f28f749f1489737f7b58c5)
Conflicts:
	src/mds/MDSRank.cc
	  - Used 'std::stringstream' instead of 'CachedStackStringStream'
	    and repllaced the 'ss.strv()' to 'ss.str()'.
(cherry picked from commit ce61c845619a9b7ed2a9bc6c63cabb670964f678)
Resolves: rhbz#1643995

src/mds/MDSRank.cc
@@ -3055,11 +3055,20 @@ bool MDSRank::evict_client(int64_t session_id,
     return false;
   }
 
+  auto& addr = session->info.inst.addr;
+  {
+    std::stringstream ss;
+    ss << "Evicting " << (blacklist ? "(and blacklisting) " : "")
+       << "client session " << session_id << " (" << addr << ")";
+    dout(1) << ss.str() << dendl;
+    clog->info() << ss.str();
+  }
+
   dout(4) << "Preparing blacklist command... (wait=" << wait << ")" << dendl;
   stringstream ss;
   ss << "{\"prefix\":\"osd blacklist\", \"blacklistop\":\"add\",";
   ss << "\"addr\":\"";
-  ss << session->info.inst.addr;
+  ss << addr;
   ss << "\"}";
   std::string tmp = ss.str();
   std::vector<std::string> cmd = {tmp};