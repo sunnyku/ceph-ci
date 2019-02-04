6949df01f65e504e7dbbcfdf5abbacc2f2cc3ace
Sage Weil <sage@redhat.com>
2019-02-04 05:30:12 -0600

    msg/Messenger: be less verbose
    This message has been annoying me
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/Messenger.cc
+++ src/msg/Messenger.cc
@@ -113,8 +113,8 @@ int get_default_crc_flags(const ConfigProxy& conf)
 
 int Messenger::bindv(const entity_addrvec_t& addrs)
 {
-  lderr(cct) << __func__ << " " << addrs << " fallback to legacy "
-	     << addrs.legacy_addr() << dendl;
+  ldout(cct,10) << __func__ << " " << addrs << " using legacy "
+		<< addrs.legacy_addr() << dendl;
   return bind(addrs.legacy_addr());
 }
 
