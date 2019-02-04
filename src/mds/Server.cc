ceph/ceph

mds: do not log internal journal error
MDS can't reach this code due to assert in C_MDS_session_finish.

Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/Server.cc
@@ -589,14 +589,8 @@ void Server::handle_client_session(const MClientSession::const_ref &m)
       sseq = mds->sessionmap.set_state(session, Session::STATE_OPENING);
       mds->sessionmap.touch_session(session);
       auto fin = new FunctionContext([log_session_status = std::move(log_session_status)](int r){
-        if (r == 0) {
-          log_session_status("ACCEPTED", "");
-        } else {
-          CachedStackStringStream _ss;
-          auto& ss = _ss.get_stream();
-          ss << "(internal) r = " << r;
-          log_session_status("REJECTED", ss.strv());
-        }
+        ceph_assert(r == 0);
+        log_session_status("ACCEPTED", "");
       });
       mdlog->start_submit_entry(new ESession(m->get_source_inst(), true, pv, client_metadata),
 				new C_MDS_session_finish(this, session, sseq, true, pv, fin));

sunnyku/ceph-ci

(cherry picked from commit 0992ac1fa62edb6143efc80a6fea7b4704107705)
Conflicts:
	src/mds/Server.cc
(cherry picked from commit 7b134974142e803171d022b8522e621c9ca09069)
Resolves: rhbz#1645693

src/mds/Server.cc
@@ -436,13 +436,8 @@ void Server::handle_client_session(MClientSession *m)
       sseq = mds->sessionmap.set_state(session, Session::STATE_OPENING);
       mds->sessionmap.touch_session(session);
       auto fin = new FunctionContext([log_session_status = std::move(log_session_status)](int r){
-        if (r == 0) {
-          log_session_status("ACCEPTED", "");
-        } else {
-          std::stringstream ss;
-          ss << "(internal) r = " << r;
-          log_session_status("REJECTED", ss.str());
-        }
+        assert(r == 0);
+        log_session_status("ACCEPTED", "");
       });
       mdlog->start_submit_entry(new ESession(m->get_source_inst(), true, pv, client_metadata),
 				new C_MDS_session_finish(this, session, sseq, true, pv, fin));