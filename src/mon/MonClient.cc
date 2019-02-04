705eac583e934fc354f794af6885d82a8dc75e7d
Sage Weil <sage@redhat.com>
2019-02-04 03:34:33 -0600

    auth: remove AUTH_UNKNOWN weirdness, hardcoded defaults.
    This is what the old code does so I kept it but I don't think it makes any sense.
    Same with the defaults; let's just set the config option to something valid.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/MonClient.cc
+++ src/mon/MonClient.cc
@@ -1514,6 +1514,9 @@ int MonConnection::get_auth_request(
   if (auth_method < 0) {
     vector<uint32_t> as;
     auth_registry->get_supported_methods(con->get_peer_type(), &as);
+    if (as.empty()) {
+      return -EACCES;
+    }
     auth_method = as.front();
   }
   *method = auth_method;
@@ -1521,6 +1524,9 @@ int MonConnection::get_auth_request(
 				     preferred_modes);
   ldout(cct,10) << __func__ << " method " << *method
 		<< " preferred_modes " << *preferred_modes << dendl;
+  if (preferred_modes->empty()) {
+    return -EACCES;
+  }
 
   if (auth) {
     auth.reset();
