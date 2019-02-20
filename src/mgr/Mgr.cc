d523df2ac362b6090d3be6ed043afbc654a5a9cf
luo.runbing <luo.runbing@zte.com.cn>
2019-02-20 14:53:08 +0800

    mgr: add DaemonServer reference for ActivePyModules
    picked from 3068c429
    
    Signed-off-by: luo.runbing <luo.runbing@zte.com.cn>

--- src/mgr/Mgr.cc
+++ src/mgr/Mgr.cc
@@ -221,7 +221,7 @@ void Mgr::init()
   // assume finisher already initialized in background_init
   dout(4) << "starting python modules..." << dendl;
   py_module_registry->active_start(loaded_config, daemon_state, cluster_state, *monc,
-      clog, *objecter, *client, finisher);
+      clog, *objecter, *client, finisher, server);
 
   dout(4) << "Complete." << dendl;
   initializing = false;
