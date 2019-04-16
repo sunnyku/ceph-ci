2d9ce3f3ebbf18077400238251aadb7a0a72ac09
Sage Weil <sage@redhat.com>
2019-04-16 14:15:26 -0500

    mgr/ActivePyModules: drop GIL while we wait for mon reply in set_store
    Fixes: http://tracker.ceph.com/issues/39335
    Fixes: http://tracker.ceph.com/issues/39040
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mgr/ActivePyModules.cc
+++ src/mgr/ActivePyModules.cc
@@ -592,6 +592,7 @@ void ActivePyModules::set_store(const std::string &module_name,
                                    + module_name + "/" + key;
   
   Command set_cmd;
+  std::ostringstream cmd_json;
   {
     PyThreadState *tstate = PyEval_SaveThread();
     std::lock_guard l(lock);
@@ -603,7 +604,6 @@ void ActivePyModules::set_store(const std::string &module_name,
       store_cache.erase(global_key);
     }
 
-    std::ostringstream cmd_json;
     JSONFormatter jf;
     jf.open_object_section("cmd");
     if (val) {
@@ -616,9 +616,15 @@ void ActivePyModules::set_store(const std::string &module_name,
     }
     jf.close_section();
     jf.flush(cmd_json);
-    set_cmd.run(&monc, cmd_json.str());
   }
-  set_cmd.wait();
+
+  // drop GIL while we send the request and block for a reply
+  {
+    PyThreadState *tstate = PyEval_SaveThread();
+    set_cmd.run(&monc, cmd_json.str());
+    set_cmd.wait();
+    PyEval_RestoreThread(tstate);
+  }
 
   if (set_cmd.r != 0) {
     // config-key set will fail if mgr's auth key has insufficient
