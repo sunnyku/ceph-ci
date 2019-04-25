e89bed801f05740a5505129d2d290a05392442d2
Adam C. Emerson <aemerson@redhat.com>
2019-04-25 16:04:46 -0400

    cls/version: Update to work with new RADOS
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/cls/version/cls_version_client.cc
+++ src/cls/version/cls_version_client.cc
@@ -2,6 +2,7 @@
 
 #include "cls/version/cls_version_client.h"
 #include "include/rados/librados.hpp"
+#include "common/waiter.h"
 
 
 using namespace librados;
@@ -98,4 +99,132 @@ int cls_version_read(librados::IoCtx& io_ctx, string& oid, obj_version *ver)
   *ver = ret.objv;
 
   return r;
+}
+
+// New World Order
+
+void cls_version_set(RADOS::WriteOp& op, obj_version& objv)
+{
+  bufferlist in;
+  cls_version_set_op call;
+  call.objv = objv;
+  encode(call, in);
+  op.exec("version", "set", in);
+}
+
+void cls_version_inc(RADOS::WriteOp& op)
+{
+  bufferlist in;
+  cls_version_inc_op call;
+  encode(call, in);
+  op.exec("version", "inc", in);
+}
+
+void cls_version_inc(RADOS::WriteOp& op, obj_version& objv, VersionCond cond)
+{
+  bufferlist in;
+  cls_version_inc_op call;
+  call.objv = objv;
+
+  obj_version_cond c;
+  c.cond = cond;
+  c.ver = objv;
+
+  call.conds.push_back(c);
+
+  encode(call, in);
+  op.exec("version", "inc_conds", in);
+}
+
+void cls_version_check(RADOS::ReadOp& op, obj_version& objv, VersionCond cond)
+{
+  bufferlist in;
+  cls_version_check_op call;
+  call.objv = objv;
+
+  obj_version_cond c;
+  c.cond = cond;
+  c.ver = objv;
+
+  call.conds.push_back(c);
+
+  encode(call, in);
+  op.exec("version", "check_conds", in, nullptr);
+}
+
+void cls_version_check(RADOS::WriteOp& op, obj_version& objv, VersionCond cond)
+{
+  bufferlist in;
+  cls_version_check_op call;
+  call.objv = objv;
+
+  obj_version_cond c;
+  c.cond = cond;
+  c.ver = objv;
+
+  call.conds.push_back(c);
+
+  encode(call, in);
+  op.exec("version", "check_conds", in);
+}
+
+void cls_version_read(RADOS::ReadOp& op, obj_version *objv)
+{
+  bufferlist inbl;
+  op.exec("version", "read", inbl,
+          [objv](boost::system::error_code ec,
+                 const bufferlist& bl) {
+            cls_version_read_ret ret;
+            if (!ec) {
+              try {
+                auto iter = bl.cbegin();
+                decode(ret, iter);
+                *objv = ret.objv;
+              } catch (const ceph::buffer::error& err) {
+                // nothing we can do about it atm
+              }
+            }
+          });
+}
+
+tl::expected<obj_version, boost::system::error_code>
+cls_version_read(RADOS::RADOS& rados, const RADOS::Object& obj,
+		 const RADOS::IOContext& ioc, optional_yield y) {
+#ifdef HAVE_BOOST_CONTEXT
+  if (y) {
+    boost::system::error_code ec;
+    auto& yield = y.get_yield_context();
+    bufferlist out;
+    RADOS::ReadOp op;
+    op.exec("version", "read", {}, &out);
+    rados.execute(obj, ioc, std::move(op),  nullptr, yield[ec]);
+    if (ec)
+      return tl::unexpected(ec);
+    cls_version_read_ret ret;
+    try {
+      auto iter = out.cbegin();
+      decode(ret, iter);
+    } catch (const ceph::buffer::error& err) {
+      return tl::unexpected(err.code());
+    }
+    return ret.objv;
+  }
+#endif
+
+  bufferlist out;
+  ceph::waiter<boost::system::error_code> w;
+  RADOS::ReadOp op;
+  op.exec("version", "read", {}, &out);
+  rados.execute(obj, ioc, std::move(op), nullptr, w.ref());
+  auto ec = w.wait();
+  if (ec)
+    return tl::unexpected(ec);
+  cls_version_read_ret ret;
+  try {
+    auto iter = out.cbegin();
+    decode(ret, iter);
+  } catch (const ceph::buffer::error& err) {
+    return tl::unexpected(err.code());
+  }
+  return ret.objv;
 }
--- src/cls/version/cls_version_client.h
+++ src/cls/version/cls_version_client.h
@@ -2,7 +2,11 @@
 #define CEPH_CLS_VERSION_CLIENT_H
 
 #include "include/rados/librados_fwd.hpp"
+#include "include/RADOS/RADOS.hpp"
 #include "cls_version_ops.h"
+
+#include "include/expected.hpp"
+#include "common/async/yield_context.h"
 
 /*
  * version objclass
@@ -21,5 +25,24 @@ void cls_version_read(librados::ObjectReadOperation& op, obj_version *objv);
 int cls_version_read(librados::IoCtx& io_ctx, string& oid, obj_version *ver);
 
 void cls_version_check(librados::ObjectOperation& op, obj_version& ver, VersionCond cond);
+
+// New World Order
+
+void cls_version_set(RADOS::WriteOp& op, obj_version& ver);
+
+/* increase anyway */
+void cls_version_inc(RADOS::WriteOp& op);
+
+/* conditional increase, return -EAGAIN if condition fails */
+void cls_version_inc(RADOS::WriteOp& op, obj_version& ver, VersionCond cond);
+
+void cls_version_read(RADOS::ReadOp& op, obj_version *objv);
+
+boost::system::error_code
+cls_version_read(RADOS::RADOS& rados, const RADOS::Object& obj,
+		 const RADOS::IOContext& ioc, obj_version *ver);
+
+void cls_version_check(RADOS::ReadOp& op, obj_version& ver, VersionCond cond);
+void cls_version_check(RADOS::WriteOp& op, obj_version& ver, VersionCond cond);
 
 #endif
