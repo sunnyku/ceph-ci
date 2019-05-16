6cd5efc00008eea91a2c4f325cfb061ecb49257d
Adam C. Emerson <aemerson@redhat.com>
2019-05-16 11:08:57 -0400

    RADOS: Use Boost.Program_Options in neorados
    Rather than rolling our own.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/tools/neorados.cc
+++ src/tools/neorados.cc
@@ -15,10 +15,12 @@
 
 #include <algorithm>
 #include <atomic>
+#include <functional>
 #include <iostream>
 #include <vector>
 
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include <boost/system/system_error.hpp>
 
 #include "include/RADOS/RADOS.hpp"
@@ -29,16 +31,16 @@ namespace R = RADOS;
 std::atomic<std::size_t> pending = 0;
 
 template<typename F>
-struct Callback {
+struct Pending {
   F f;
-  Callback(F&& f) : f(std::forward<F>(f)) {
+  Pending(F&& f) : f(std::forward<F>(f)) {
     ++pending;
   }
 
   template<typename... Args>
   void operator()(Args&&... args) {
-    --pending;
     std::move(f)(std::forward<Args>(args)...);
+    --pending;
   }
 };
 
@@ -62,65 +64,127 @@ std::ostream& printseq(const V& v, std::ostream& m, F&& f) {
 
 template<typename F>
 void lookup_pool(R::RADOS& r, const std::string& pname, F&& f) {
-  ++pending;
   r.lookup_pool(pname,
-		[f = std::move(f)](bs::error_code ec, std::int64_t p) {
-		  --pending;
-		  if (ec)
-		    throw bs::system_error(ec);
-		  std::move(f)(p);
-		});
+		Pending([f = std::move(f)](bs::error_code ec, std::int64_t p)
+			mutable {
+			  if (ec)
+			    throw bs::system_error(ec);
+			  std::move(f)(p);
+			}));
 }
 
 
 void lspools(R::RADOS& r) {
-  ++pending;
-  r.list_pools([](std::vector<std::pair<std::int64_t, std::string>>&& l) {
-		 --pending;
-		 printseq(l,
-			  std::cout, [](const auto& p) -> const std::string& {
-				       return p.second;
-				     });
-	       });
+  r.list_pools(
+    Pending([](std::vector<std::pair<std::int64_t, std::string>>&& l) {
+	      printseq(l,
+		       std::cout, [](const auto& p) -> const std::string& {
+				    return p.second;
+				  });
+	    }));
 }
+
+struct Ls : public Pending<std::reference_wrapper<Ls>> {
+  R::RADOS& r;
+  std::int64_t p;
+
+  Ls(R::RADOS& r, std::int64_t p)
+    : Pending(std::ref(*this)), r(r),  p(p) {}
+
+  void operator()(bs::error_code ec, std::vector<R::EnumeratedObject> ls,
+		  R::EnumerationCursor next) {
+    printseq(ls, std::cout);
+    if (next != R::EnumerationCursor::end()) {
+      r.enumerate_objects(
+	p, R::EnumerationCursor::begin(),
+	R::EnumerationCursor::end(), 1000, {},
+	Ls(r, p), R::all_nspaces);
+    }
+  }
+};
 
 void ls(R::RADOS& r, const std::string& pname) {
-  ++pending;
   lookup_pool(r, pname,
-	      [&r](std::int64_t p) {
-		--pending;
-		auto next = std::make_unique<R::EnumerationCursor>();
-		auto v = std::make_unique<std::vector<R::EnumeratedObject>>();
-		++pending;
-		r.enumerate_objects(
-		  p, R::EnumerationCursor::begin(),
-		  R::EnumerationCursor::end(), 1000, {}, v.get(), next.get(),
-		  [v = std::move(v), next = std::move(next)]
-		  (bs::error_code ec) {
-		    --pending;
-		    printseq(*v, std::cout);
-		  }, R::all_nspaces);
-	      });
+	      Pending(
+		[&r](std::int64_t p) {
+		  r.enumerate_objects(
+		    p, R::EnumerationCursor::begin(),
+		    R::EnumerationCursor::end(), 1000, {},
+		    Ls(r, p), R::all_nspaces);
+		}));
 }
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  boost::asio::io_context c;
-  auto r = RADOS::RADOS::Builder{}.build(c);
-
-  // Do this properly later.
-
+  namespace po = boost::program_options;
   try {
-    if (argc == 0) {
+    std::string command;
+    std::vector<std::string> parameters;
+
+    po::options_description desc("neorados options");
+    desc.add_options()
+      ("help", "show help")
+      ("version", "show version")
+      ("command", po::value<std::string>(&command), "the operation to perform")
+      ("parameters", po::value<std::vector<std::string>>(&parameters),
+       "parameters to the command");
+
+    po::positional_options_description p;
+    p.add("command", 1);
+    p.add("parameters", -1);
+
+    po::variables_map vm;
+
+    po::store(po::command_line_parser(argc, argv).
+	      options(desc).positional(p).run(), vm);
+
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc;
+      return 0;
+    }
+
+    if (vm.count("version")) {
+      std::cout
+	<< "neorados: RADOS command exerciser, v0.0.1\n"
+	<< "Copyright (C) 2019 Red Hat <contact@redhat.com>\n"
+	<< "This is free software; you can redistribute it and/or\n"
+	<< "modify it under the terms of the GNU Lesser General Public\n"
+	<< "License version 2.1, as published by the Free Software\n"
+	<< "Foundation.  See file COPYING." << std::endl;
+      return 0;
+    }
+
+    if (vm.find("command") == vm.end()) {
+      std::cerr << "A command is required." << std::endl;
       return 1;
     }
-    if (argv[1] == "lspools"sv) {
+
+    boost::asio::io_context c;
+    auto r = RADOS::RADOS::Builder{}.build(c);
+
+    // Do this properly later.
+
+    if (command == "lspools"sv) {
+      if (!parameters.empty()) {
+	std::cerr << "lspools takes no parameters" << std::endl;
+	return 1;
+      }
       lspools(r);
-    } else if (argv[1] == "ls"sv) {
-      if (argc < 2)
-        return 1;
-      ls(r, argv[2]);
-    } else {
+    }
+    else if (command == "ls"sv) {
+      if (parameters.empty()) {
+	std::cerr << "ls requires pool argument" << std::endl;
+	return 1;
+      } else if (parameters.size() > 1) {
+	std::cerr << "ls requires exactly more than one argument" << std::endl;
+	return 1;
+      }
+      ls(r, parameters[1]);
+    }
+    else {
+      std::cerr << "unknown command: " << command << std::endl;
       return 1;
     }
     while (pending) {
