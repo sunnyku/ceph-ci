0fdcedc8e7573a96938a200dc1be45950510f66c
Adam C. Emerson <aemerson@redhat.com>
2019-05-24 16:27:13 -0400

    neorados: Refactor and more
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/tools/neorados.cc
+++ src/tools/neorados.cc
@@ -15,47 +15,60 @@
 
 #include <algorithm>
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <tuple>
 #include <vector>
 
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 #include <boost/system/system_error.hpp>
 
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
 #include "include/RADOS/RADOS.hpp"
+
+using namespace std::literals;
 
 namespace bs = boost::system;
 namespace R = RADOS;
 
+std::string verstr(const std::tuple<uint32_t, uint32_t, uint32_t>& v) {
+  auto [maj, min, p] = v;
+  return fmt::format("v{}.{}.{}", maj, min, p);
+}
+
 template<typename V>
-std::ostream& printseq(const V& v, std::ostream& m) {
+void printseq(const V& v, std::ostream& m) {
   std::for_each(v.cbegin(), v.cend(),
 		[&m](const auto& e) {
-		  m << e << std::endl;
+		  fmt::print(m, "{}\n", e);
 		});
-  return m;
 }
 
 template<typename V, typename F>
-std::ostream& printseq(const V& v, std::ostream& m, F&& f) {
+void printseq(const V& v, std::ostream& m, F&& f) {
   std::for_each(v.cbegin(), v.cend(),
 		[&m, &f](const auto& e) {
-		  m << f(e) << std::endl;
+		  fmt::print(m, "{}\n", f(e));
 		});
-  return m;
 }
 
 template<typename F>
 void lookup_pool(R::RADOS& r, const std::string& pname, F&& f) {
   r.lookup_pool(pname,
-		[f = std::move(f)](bs::error_code ec, std::int64_t p) mutable {
+		[f = std::move(f), pname = std::move(pname)]
+		(bs::error_code ec, std::int64_t p) mutable {
 		  if (ec)
-		    throw bs::system_error(ec);
+		    throw bs::system_error(
+		      ec, fmt::format("when looking up '{}'", pname));
 		  std::move(f)(p);
 		});
 }
 
 
-void lspools(R::RADOS& r) {
+void lspools(R::RADOS& r, const std::vector<std::string>&) {
   r.list_pools(
     [](std::vector<std::pair<std::int64_t, std::string>>&& l) {
       printseq(l, std::cout, [](const auto& p) -> const std::string& {
@@ -64,43 +77,105 @@ void lspools(R::RADOS& r) {
     });
 }
 
-struct Ls  {
+struct Ls {
   R::RADOS& r;
   std::int64_t p;
+  std::string pname;
 
-  Ls(R::RADOS& r, std::int64_t p)
-    : r(r), p(p) {}
+  Ls(R::RADOS& r, std::int64_t p, std::string&& pname)
+    : r(r), p(p), pname(std::move(pname)) {}
 
   void operator()(bs::error_code ec, std::vector<R::EnumeratedObject> ls,
 		  R::EnumerationCursor next) {
+    if (ec)
+      throw bs::system_error(ec, fmt::format("when listing {}", pname));
+
     printseq(ls, std::cout);
     if (next != R::EnumerationCursor::end()) {
       r.enumerate_objects(
-	p, R::EnumerationCursor::begin(),
+	p, next,
 	R::EnumerationCursor::end(), 1000, {},
-	Ls(r, p), R::all_nspaces);
+	Ls(r, p, std::move(pname)), R::all_nspaces);
     }
   }
 };
 
-void ls(R::RADOS& r, const std::string& pname) {
+void ls(R::RADOS& r, const std::vector<std::string>& p) {
+  const auto& pname = p[0];
   lookup_pool(r, pname,
-	      [&r](std::int64_t p) {
+	      [&r, pname = std::string(pname)](std::int64_t p) mutable {
 		r.enumerate_objects(
 		  p, R::EnumerationCursor::begin(),
 		  R::EnumerationCursor::end(), 1000, {},
-		  Ls(r, p), R::all_nspaces);
+		  Ls(r, p, std::move(pname)), R::all_nspaces);
 	      });
 }
 
+void mkpool(R::RADOS& r, const std::vector<std::string>& p) {
+  auto pname = p[0];
+  r.create_pool(pname, std::nullopt,
+		[pname](bs::error_code ec) {
+		  if (ec)
+		    throw bs::system_error(
+		      ec, fmt::format("when creating '{}'", pname));
+		});
+}
+
+void rmpool(R::RADOS& r, const std::vector<std::string>& p) {
+  auto pname = p[0];
+  r.delete_pool(pname,
+		[pname](bs::error_code ec) {
+		  if (ec)
+		    throw bs::system_error(
+		      ec, fmt::format("when removing '{}'", pname));
+		});
+}
+
+static constexpr auto version = std::make_tuple(0ul, 0ul, 1ul);
+
+using cmdfunc = void (*)(R::RADOS& r, const std::vector<std::string>& p);
+
+struct cmdesc {
+  std::string_view name;
+  std::size_t arity;
+  cmdfunc f;
+  std::string_view usage;
+  std::string_view desc;
+};
+
+const std::array commands = {
+  // Pools operations ;)
+
+  cmdesc{ "lspools"sv,
+	  0, &lspools,
+	  ""sv,
+	  "List all pools"sv },
+
+  // Pool operations
+
+  cmdesc{ "ls"sv,
+	  1, &ls,
+	  "POOL"sv,
+	  "list all objects in POOL"sv },
+  cmdesc{ "mkpool"sv,
+	  1, &mkpool,
+	  "POOL"sv,
+	  "create POOL"sv },
+  cmdesc{ "rmpool"sv,
+	  1, &rmpool,
+	  "POOL"sv,
+	  "remove POOL"sv }
+};
+
 int main(int argc, char* argv[])
 {
+  std::string_view prog(argv[0]);
+  std::string command;
   namespace po = boost::program_options;
   try {
-    std::string command;
     std::vector<std::string> parameters;
 
-    po::options_description desc("neorados options");
+    po::options_description desc(fmt::format("{} options", prog));
     desc.add_options()
       ("help", "show help")
       ("version", "show version")
@@ -120,53 +195,58 @@ int main(int argc, char* argv[])
     po::notify(vm);
 
     if (vm.count("help")) {
-      std::cout << desc;
+      fmt::print("{}", desc);
+      fmt::print("\nCommands:\n");
+      for (const auto& cmd : commands) {
+	fmt::print("\t{} {}\n\t\t{}\n",
+		   cmd.name, cmd.usage, cmd.desc);
+      }
       return 0;
     }
 
     if (vm.count("version")) {
-      std::cout
-	<< "neorados: RADOS command exerciser, v0.0.1\n"
-	<< "Copyright (C) 2019 Red Hat <contact@redhat.com>\n"
-	<< "This is free software; you can redistribute it and/or\n"
-	<< "modify it under the terms of the GNU Lesser General Public\n"
-	<< "License version 2.1, as published by the Free Software\n"
-	<< "Foundation.  See file COPYING." << std::endl;
+      fmt::print(
+	"{}: RADOS command exerciser, {},\n"
+	"RADOS library version {}\n"
+	"Copyright (C) 2019 Red Hat <contact@redhat.com>\n"
+	"This is free software; you can redistribute it and/or\n"
+	"modify it under the terms of the GNU Lesser General Public\n"
+	"License version 2.1, as published by the Free Software\n"
+	"Foundation.  See file COPYING.\n", prog,
+	verstr(version), verstr(R::RADOS::version()));
       return 0;
     }
 
     if (vm.find("command") == vm.end()) {
-      std::cerr << "A command is required." << std::endl;
+      fmt::print(std::cerr, "{}: A command is required\n", prog);
       return 1;
     }
 
     boost::asio::io_context c;
     auto r = RADOS::RADOS::Builder{}.build(c);
 
-    if (command == "lspools"sv) {
-      if (!parameters.empty()) {
-	std::cerr << "lspools takes no parameters" << std::endl;
+    if (auto ci = std::find_if(commands.begin(), commands.end(),
+			       [&command](const cmdesc& c) {
+				 return c.name == command;
+			       }); ci != commands.end()) {
+      if (parameters.size() < ci->arity) {
+	fmt::print(std::cerr, "{}: {}: too few arguments\n\t{} {}\n",
+		   prog, command, ci->name, ci->usage);
 	return 1;
       }
-      lspools(r);
-    }
-    else if (command == "ls"sv) {
-      if (parameters.empty()) {
-	std::cerr << "ls requires pool argument" << std::endl;
-	return 1;
-      } else if (parameters.size() > 1) {
-	std::cerr << "ls requires exactly more than one argument" << std::endl;
+      if (parameters.size() > ci->arity) {
+	fmt::print(std::cerr, "{}: {}: too many arguments\n\t{} {}\n",
+		   prog, command, ci->name, ci->usage);
 	return 1;
       }
-      ls(r, parameters[1]);
-    }
-    else {
-      std::cerr << "unknown command: " << command << std::endl;
+      ci->f(r, parameters);
+    } else {
+      fmt::print(std::cerr, "{}: {}: unknown command\n", prog, command);
       return 1;
     }
     c.run();
   } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
+    fmt::print(std::cerr, "{}: {}: {}\n", prog, command, e.what());
     return 1;
   }
 
