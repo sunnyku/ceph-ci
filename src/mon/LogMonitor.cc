ceph/ceph

mon/LogMonitor: lower logging level for check_sub
Signed-off-by: runsisi <luo.runbing@zte.com.cn>

src/mon/LogMonitor.cc
@@ -640,7 +640,7 @@ void LogMonitor::check_sub(Subscription *s)
     _create_sub_incremental(mlog, sub_level, s->next);
   }
 
-  dout(1) << __func__ << " sending message to " << s->session->name
+  dout(10) << __func__ << " sending message to " << s->session->name
 	  << " with " << mlog->entries.size() << " entries"
 	  << " (version " << mlog->version << ")" << dendl;
   

sunnyku/ceph-ci

(cherry picked from commit 0ca5dbebd1fa211d4e203955417ca339fe3814cd)
Conflicts:
	src/mon/LogMonitor.cc

src/mon/LogMonitor.cc
@@ -588,7 +588,7 @@ void LogMonitor::check_sub(Subscription *s)
     _create_sub_incremental(mlog, sub_level, s->next);
   }
 
-  dout(1) << __func__ << " sending message to " << s->session->inst 
+  dout(10) << __func__ << " sending message to " << s->session->inst
 	  << " with " << mlog->entries.size() << " entries"
 	  << " (version " << mlog->version << ")" << dendl;
   