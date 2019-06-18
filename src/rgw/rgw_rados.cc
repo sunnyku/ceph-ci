5ee0d83ed9d50f8d28f001e108aece801b77bf5c
Matt Benjamin <mbenjamin@redhat.com>
2019-06-18 16:12:55 -0400

    rgw: s/std::map/boost::container::flat_map/ cls_bucket_list_ordered
    Probably faster, allocating less.  Definitely not slower.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -2457,7 +2457,9 @@ int RGWRados::Bucket::List::list_objects_ordered(
 		     << "[" << cur_marker.instance << "]"
 		     << dendl;
     }
-    std::map<string, rgw_bucket_dir_entry> ent_map;
+
+    ent_map_t ent_map;
+    ent_map.reserve(read_ahead);
     int r = store->cls_bucket_list_ordered(target->get_bucket_info(),
 					   shard_id,
 					   cur_marker,
@@ -9115,7 +9117,7 @@ int RGWRados::cls_bucket_list_ordered(RGWBucketInfo& bucket_info,
 				      const string& prefix,
 				      uint32_t num_entries,
 				      bool list_versions,
-				      map<string, rgw_bucket_dir_entry>& m,
+				      ent_map_t& m,
 				      bool *is_truncated,
 				      rgw_obj_index_key *last_entry,
                                       optional_yield y,
@@ -9143,8 +9145,8 @@ int RGWRados::cls_bucket_list_ordered(RGWBucketInfo& bucket_info,
     return r;
 
   // Create a list of iterators that are used to iterate each shard
-  vector<map<string, struct rgw_bucket_dir_entry>::iterator> vcurrents;
-  vector<map<string, struct rgw_bucket_dir_entry>::iterator> vends;
+  vector<RGWRados::ent_map_t::iterator> vcurrents;
+  vector<RGWRados::ent_map_t::iterator> vends;
   vector<string> vnames;
   vcurrents.reserve(list_results.size());
   vends.reserve(list_results.size());
