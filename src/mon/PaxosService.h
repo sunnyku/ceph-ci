liewegas/ceph

mon/PaxosService: add C_ReplyOp
Generic helper context to reply to an op on (successful) finish.

Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/PaxosService.h
@@ -124,6 +124,20 @@ public:
     }
   };
 
+  class C_ReplyOp : public C_MonOp {
+    Monitor *mon;
+    MonOpRequestRef op;
+    MessageRef reply;
+  public:
+    C_ReplyOp(PaxosService *s, MonOpRequestRef o, MessageRef r) :
+      C_MonOp(op), mon(s->mon), op(o), reply(r) { }
+    void _finish(int r) override {
+      if (r >= 0) {
+	mon->send_reply(op, reply.detach());
+      }
+    }
+  };
+
   /**
    * @}
    */

sunnyku/ceph-ci

src/mon/PaxosService.h
@@ -124,6 +124,20 @@ public:
     }
   };
 
+  class C_ReplyOp : public C_MonOp {
+    Monitor *mon;
+    MonOpRequestRef op;
+    MessageRef reply;
+  public:
+    C_ReplyOp(PaxosService *s, MonOpRequestRef o, MessageRef r) :
+      C_MonOp(o), mon(s->mon), op(o), reply(r) { }
+    void _finish(int r) override {
+      if (r >= 0) {
+	mon->send_reply(op, reply.detach());
+      }
+    }
+  };
+
   /**
    * @}
    */