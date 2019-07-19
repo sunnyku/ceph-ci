da070f61d5a161712f75bd811f298e7cfc48262e
Sage Weil <sage@redhat.com>
2019-07-19 16:33:52 -0500

    osd: add lease/readable tracking members, helpers
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -358,6 +358,7 @@ void PeeringState::advance_map(
   if (pool.info.last_change == osdmap_ref->get_epoch()) {
     pl->on_pool_change();
   }
+  readable_interval = pool.get_readable_interval();
   last_require_osd_release = osdmap->require_osd_release;
 }
 
@@ -688,6 +689,14 @@ void PeeringState::on_new_interval()
 
   init_hb_stamps();
 
+  acting_readable_until_ub.clear();
+  if (is_primary()) {
+    acting_readable_until_ub.resize(acting.size(), ceph::signedspan::zero());
+
+    // start lease here, so that we get acks during peering
+    renew_lease(pl->get_mnow());
+  }
+
   pl->on_new_interval();
 }
 
@@ -1012,6 +1021,23 @@ bool PeeringState::set_force_backfill(bool b)
     pl->update_local_background_io_priority(get_backfill_priority());
   }
   return did;
+}
+
+void PeeringState::recalc_readable_until()
+{
+  assert(is_primary());
+  ceph::signedspan min = readable_until_ub;
+  for (unsigned i = 0; i < acting.size(); ++i) {
+    if (acting[i] == pg_whoami.osd) {
+      continue;
+    }
+    if (acting_readable_until_ub[i] < min) {
+      min = acting_readable_until_ub[i];
+    }
+  }
+  readable_until = min;
+  dout(20) << __func__ << " readable_until " << readable_until
+	   << " readable_until_ub " << readable_until_ub << dendl;
 }
 
 bool PeeringState::adjust_need_up_thru(const OSDMapRef osdmap)
