yehudasa/ceph-pr-archive

RADOS_unleashed: Operate
Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rados_unleashed/RADOSImpl.h
@@ -36,10 +36,12 @@
 
 
 namespace RADOS_unleashed {
+  class RADOS;
 namespace _ {
 
 class RADOS
 {
+  friend ::RADOS_unleashed::RADOS;
   struct MsgDeleter {
     void operator()(Messenger* p) const {
       if (p) {

sunnyku/ceph-ci

src/RADOS/RADOSImpl.h
@@ -36,10 +36,12 @@
 
 
 namespace RADOS {
+  class RADOS;
 namespace _ {
 
 class RADOS
 {
+  friend ::RADOS::RADOS;
   struct MsgDeleter {
     void operator()(Messenger* p) const {
       if (p) {