df871d168092a89642b6fe27553aa7819a81803d
Igor Fedotov <ifedotov@suse.com>
2019-10-11 17:34:58 +0300

    os/bluestore: be more verbose in _open_super_meta by default.
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -10324,7 +10324,7 @@ int BlueStore::_open_super_meta()
       derr << __func__ << " unable to read nid_max" << dendl;
       return -EIO;
     }
-    dout(10) << __func__ << " old nid_max " << nid_max << dendl;
+    dout(1) << __func__ << " old nid_max " << nid_max << dendl;
     nid_last = nid_max.load();
   }
 
@@ -10342,7 +10342,7 @@ int BlueStore::_open_super_meta()
       derr << __func__ << " unable to read blobid_max" << dendl;
       return -EIO;
     }
-    dout(10) << __func__ << " old blobid_max " << blobid_max << dendl;
+    dout(1) << __func__ << " old blobid_max " << blobid_max << dendl;
     blobid_last = blobid_max.load();
   }
 
@@ -10352,7 +10352,7 @@ int BlueStore::_open_super_meta()
     db->get(PREFIX_SUPER, "freelist_type", &bl);
     if (bl.length()) {
       freelist_type = std::string(bl.c_str(), bl.length());
-      dout(10) << __func__ << " freelist_type " << freelist_type << dendl;
+      dout(1) << __func__ << " freelist_type " << freelist_type << dendl;
     } else {
       ceph_abort_msg("Not Support extent freelist manager");
     }
@@ -10390,7 +10390,7 @@ int BlueStore::_open_super_meta()
 	}
       }
     }
-    dout(10) << __func__ << " ondisk_format " << ondisk_format
+    dout(1) << __func__ << " ondisk_format " << ondisk_format
 	     << " compat_ondisk_format " << compat_ondisk_format
 	     << dendl;
   }
@@ -10416,7 +10416,7 @@ int BlueStore::_open_super_meta()
       derr << __func__ << " unable to read min_alloc_size" << dendl;
       return -EIO;
     }
-    dout(10) << __func__ << " min_alloc_size 0x" << std::hex << min_alloc_size
+    dout(1) << __func__ << " min_alloc_size 0x" << std::hex << min_alloc_size
 	     << std::dec << dendl;
   }
 
@@ -10425,9 +10425,9 @@ int BlueStore::_open_super_meta()
     db->get(PREFIX_SUPER, "per_pool_omap", &bl);
     if (bl.length()) {
       per_pool_omap = true;
-      dout(10) << __func__ << " per_pool_omap=1" << dendl;
+      dout(1) << __func__ << " per_pool_omap=1" << dendl;
     } else {
-      dout(10) << __func__ << " per_pool_omap not present" << dendl;
+      dout(1) << __func__ << " per_pool_omap not present" << dendl;
     }
     _check_no_per_pool_omap_alert();
   }
