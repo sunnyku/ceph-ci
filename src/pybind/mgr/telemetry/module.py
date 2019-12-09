ceph/ceph

mgr/telemetry: include min_mon_release and msgr v1 vs v2 addr count
Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/telemetry/module.py
@@ -371,9 +371,28 @@ class Module(MgrModule):
 
             report['created'] = mon_map['created']
 
+            v1_mons = 0
+            v2_mons = 0
+            ipv4_mons = 0
+            ipv6_mons = 0
+            for mon in mon_map['mons']:
+                for a in mon['public_addrs']['addrvec']:
+                    if a['type'] == 'v2':
+                        v2_mons += 1
+                    elif a['type'] == 'v1':
+                        v1_mons += 1
+                    if a['addr'].startswith('['):
+                        ipv6_mons += 1
+                    else:
+                        ipv4_mons += 1
             report['mon'] = {
                 'count': len(mon_map['mons']),
-                'features': mon_map['features']
+                'features': mon_map['features'],
+                'min_mon_release': mon_map['min_mon_release'],
+                'v1_addr_mons': v1_mons,
+                'v2_addr_mons': v2_mons,
+                'ipv4_addr_mons': ipv4_mons,
+                'ipv6_addr_mons': ipv6_mons,
             }
 
             report['config'] = self.gather_configs()

sunnyku/ceph-ci

(cherry picked from commit 3453930d438dc3ba9ba5addca59aec6786293bd4)
 Note:
    This commit was heavily modified. We wanted to provide the number of
    ipv4 and ipv6 monitors in the report, so we rewrote that part so we
    can report on it; but we had to drop everything else (msgr1 and
    msgr2), as well as 'min_mon_release'. Those do not exist in
    luminous. In the end, the commit message itself is misleading, but
    we are somehow (*shrug*) opting for leaving the commit as the original.

src/pybind/mgr/telemetry/module.py
@@ -297,9 +297,19 @@ class Module(MgrModule):
 
             report['created'] = mon_map['created']
 
+            ipv4_mons = 0
+            ipv6_mons = 0
+            for mon in mon_map['mons']:
+                if mon['public_addr'].startswith('['):
+                    ipv6_mons += 1
+                else:
+                    ipv4_mons += 1
+
             report['mon'] = {
                 'count': len(mon_map['mons']),
-                'features': mon_map['features']
+                'features': mon_map['features'],
+                'ipv4_addr_mons': ipv4_mons,
+                'ipv6_addr_mons': ipv6_mons,
             }
 
             num_pg = 0