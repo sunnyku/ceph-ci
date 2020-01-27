ecbfda02e552484649f385b46ac26ada9d577145
Sage Weil <sage@redhat.com>
2020-01-27 08:59:40 -0600

    mgr/cephadm: fix style of placeholder * services entry
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -1186,7 +1186,7 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
                         'name': '*',
                         'service_type': '*',
                         'service_instance': '*',
-                        'style': 'cephadm',
+                        'style': 'cephadm:v1',
                         'fsid': self._cluster_fsid,
                     },
                 ]
