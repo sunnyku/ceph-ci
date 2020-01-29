9587f692eeed207e348c7d98c91736d92a88a200
Greg Farnum <gfarnum@redhat.com>
2020-01-29 04:51:57 -0800

    mon: elector: add some flexibility when dropping out-of-quorum propose messages
    I haven't seen it be an issue, but I'm worried a slight different in ping
    report timing might result in flapping leaders even with the new
    ignore-out-of-quorum code.
    Imagine DCs A, B, C where A and B are netsplit: C might first elect A, then
    get a propose from B immediately following a successful ping reply that gives
    it a better score than A and thus gets an election win; then A could do
    the same, etc.
    
    In a default 12-hour halflife, 2-second ping config, the most a single ping
    can change the score is by 0.00002314814. Therefore a code default of .0001
    and a config default of .0005 should be plenty of room to prevent that in
    sane monitor configurations, while still responding quickly if connections are
    restored.
    
    Plus of course this only applies to out-of-quorum monitors to peons, so if
    a monitor manages to contact the leader they will be allowed to join
    instantly.
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- qa/workunits/cephtool/test.sh
+++ qa/workunits/cephtool/test.sh
@@ -1168,11 +1168,11 @@ function test_mon_mon()
   expect_false ceph mon feature set abcd --yes-i-really-mean-it
 
   expect_failure ceph mon add disallowed_leader $first
-  ceph mon set election_strategy DISALLOW
+  ceph mon set election_strategy disallow
   ceph mon add disallowed_leader $first
-  ceph mon set election_strategy CONNECTIVITY
+  ceph mon set election_strategy connectivity
   ceph mon rm disallowed_leader $first
-  ceph mon set election_strategy CLASSIC
+  ceph mon set election_strategy classic
   expect_failure ceph mon rm disallowed_leader $first
 }
 
