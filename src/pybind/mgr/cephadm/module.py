ceph/ceph

commit 82e4f1e0f1332f7d4f27dd0f83d394a6f14c6bd2

mgr/cephadm: remove inventory_cache
No longer used.

Clean up _get_hosts now that there is only one user.

Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/cephadm/module.py
@@ -535,27 +535,14 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
             self.inventory = dict()
         self.log.debug('Loaded inventory %s' % self.inventory)
 
-        # The values are cached by instance.
-        # cache is invalidated by
-        # 1. timeout
-        # 2. refresh parameter
-        self.inventory_cache = orchestrator.OutdatablePersistentDict(
-            self, self._STORE_HOST_PREFIX + '.devices')
-
         self.cache = HostCache(self)
         self.cache.load()
 
         # ensure the host lists are in sync
         for h in self.inventory.keys():
-            if h not in self.inventory_cache:
-                self.log.debug('adding inventory item for %s' % h)
-                self.inventory_cache[h] = orchestrator.OutdatableData()
             if h not in self.cache.daemons:
                 self.log.debug('adding service item for %s' % h)
                 self.cache.prime_empty_host(h)
-        for h in self.inventory_cache:
-            if h not in self.inventory:
-                del self.inventory_cache[h]
         for h in self.cache.get_hosts():
             if h not in self.inventory:
                 self.cache.rm_host(h)
@@ -1092,7 +1079,7 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
         """
         if isinstance(hosts, six.string_types):
             hosts = [hosts]
-        keys = self.inventory_cache.keys()
+        keys = self.inventory.keys()
         unregistered_hosts = set(hosts) - keys
         if unregistered_hosts:
             logger.warning('keys = {}'.format(keys))
@@ -1317,8 +1304,8 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
             self.log.exception(ex)
             raise
 
-    def _get_hosts(self, wanted=None):
-        return self.inventory_cache.items_filtered(wanted)
+    def _get_hosts(self):
+        return [a for a in self.inventory.keys()]
 
     @async_completion
     def add_host(self, spec):
@@ -1342,7 +1329,6 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
             'labels': spec.labels,
         }
         self._save_inventory()
-        self.inventory_cache[spec.hostname] = orchestrator.OutdatableData()
         self.cache.prime_empty_host(spec.hostname)
         self.event.set()  # refresh stray health check
         return "Added host '{}'".format(spec.hostname)
@@ -1357,7 +1343,6 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
         """
         del self.inventory[host]
         self._save_inventory()
-        del self.inventory_cache[host]
         self.cache.rm_host(host)
         self._reset_con(host)
         self.event.set()  # refresh stray health check
@@ -2281,7 +2266,7 @@ scrape_configs:
     def _get_container_image_id(self, image_name):
         # pick a random host...
         host = None
-        for host_name in self.inventory_cache:
+        for host_name, hi in self.inventory.items():
             host = host_name
             break
         if not host:

sunnyku/ceph-ci

commit efa4e72ba2585637be80e2cbdd0666948ef614fc

mgr/cephadm: fix NodePlacement host names
This broke back in 82e4f1e0f1332f7d4f27dd0f83d394a6f14c6bd2, I think.

Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/cephadm/module.py
@@ -2574,7 +2574,7 @@ class NodeAssignment(object):
         if not self.spec.placement.label and not self.spec.placement.hosts and self.spec.placement.count:
             logger.info("Found num spec. Looking for labeled nodes.")
             # TODO: actually query for labels (self.daemon_type)
-            candidates = self.scheduler.place([x[0] for x in self.get_hosts_func()],
+            candidates = self.scheduler.place([x for x in self.get_hosts_func()],
                                               count=self.spec.placement.count)
             # Not enough nodes to deploy on
             if len(candidates) != self.spec.placement.count:
@@ -2586,7 +2586,7 @@ class NodeAssignment(object):
                 self.spec.placement.set_hosts(candidates)
                 return None
 
-            candidates = self.scheduler.place([x[0] for x in self.get_hosts_func()], count=self.spec.placement.count)
+            candidates = self.scheduler.place([x for x in self.get_hosts_func()], count=self.spec.placement.count)
             # Not enough nodes to deploy on
             if len(candidates) != self.spec.placement.count:
                 raise OrchestratorValidationError("Cannot place {} daemons on {} hosts.".