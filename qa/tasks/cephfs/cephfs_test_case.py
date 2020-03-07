sidharthanup/ceph

qa: add tests for ephemeral pinning
Signed-off-by: Sidharth Anupkrishnan <sanupkri@redhat.com>

qa/tasks/cephfs/cephfs_test_case.py
@@ -304,3 +304,44 @@ class CephFSTestCase(CephTestCase):
                 return subtrees
             time.sleep(pause)
         raise RuntimeError("rank {0} failed to reach desired subtree state".format(rank))
+
+    def _wait_distributed_subtrees(self, status, rank, count):
+        timeout = 30
+        pause = 2
+        for i in range(timeout/pause):
+            subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+            subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+            subtrees = filter(lambda s: s['is_export_ephemeral_distributed_pinned'] == 1, subtrees)
+
+            if (len(subtrees) == count):
+                return subtrees
+            time.sleep(pause)
+        raise RuntimeError("rank {0} failed to reach desired subtree state".format(rank))
+
+    def get_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: s['auth_first'] == rank, subtrees)
+
+        return subtrees
+
+    def get_ephemerally_pinned_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: s['is_export_ephemeral_distributed_pinned'] == 1 || s['is_export_ephemeral_random_pinned'] == 1, subtrees)
+
+        return subtrees
+
+    def get_distributed_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: (s['is_export_ephemeral_distributed_pinned'] == 1) and (s['auth_first'] == rank), subtrees)
+
+        return subtrees
+
+    def get_random_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: (s['is_export_ephemeral_random_pinned'] == 1) and (s['auth_first'] == rank), subtrees)
+
+        return subtrees

sunnyku/ceph-ci

qa/tasks/cephfs/cephfs_test_case.py
@@ -304,3 +304,44 @@ class CephFSTestCase(CephTestCase):
                 return subtrees
             time.sleep(pause)
         raise RuntimeError("rank {0} failed to reach desired subtree state".format(rank))
+
+    def _wait_distributed_subtrees(self, status, rank, count):
+        timeout = 30
+        pause = 2
+        for i in range(timeout/pause):
+            subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+            subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+            subtrees = filter(lambda s: s['distributed_ephemeral_pin'] == 1, subtrees)
+
+            if (len(subtrees) == count):
+                return subtrees
+            time.sleep(pause)
+        raise RuntimeError("rank {0} failed to reach desired subtree state".format(rank))
+
+    def get_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: s['auth_first'] == rank, subtrees)
+
+        return subtrees
+
+    def get_ephemerally_pinned_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: (s['distributed_ephemeral_pin'] == 1 or s['random_ephemeral_pin'] == 1) and (s['auth_first'] == rank), subtrees)
+
+        return subtrees
+
+    def get_distributed_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: (s['distributed_ephemeral_pin'] == 1) and (s['auth_first'] == rank), subtrees)
+
+        return subtrees
+
+    def get_random_auth_subtrees(self, status, rank):
+        subtrees = self.fs.mds_asok(["get", "subtrees"], mds_id=status.get_rank(self.fs.id, rank)['name'])
+        subtrees = filter(lambda s: s['dir']['path'].startswith('/'), subtrees)
+        subtrees = filter(lambda s: (s['random_ephemeral_pin'] == 1) and (s['auth_first'] == rank), subtrees)
+
+        return subtrees