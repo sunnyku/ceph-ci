c5972a06bda3ab6ec0368cc34f3e7426c755b95c
Michael Fritch <mfritch@suse.com>
2020-07-28 13:57:17 -0600

    mgr/orch: allow for multiline OrchestratorEvent message
    Signed-off-by: Michael Fritch <mfritch@suse.com>

--- src/pybind/mgr/orchestrator/_interface.py
+++ src/pybind/mgr/orchestrator/_interface.py
@@ -1604,7 +1604,7 @@ class OrchestratorEvent:
     """
     INFO = 'INFO'
     ERROR = 'ERROR'
-    regex_v1 = re.compile(r'^([^ ]+) ([^:]+):([^ ]+) \[([^\]]+)\] "(.*)"$')
+    regex_v1 = re.compile(r'^([^ ]+) ([^:]+):([^ ]+) \[([^\]]+)\] "((?:.|\n)*)"$', re.MULTILINE)
 
     def __init__(self, created: Union[str, datetime.datetime], kind, subject, level, message):
         if isinstance(created, str):
@@ -1632,7 +1632,6 @@ class OrchestratorEvent:
         # Make a long list of events readable.
         created = self.created.strftime(DATEFMT)
         return f'{created} {self.kind_subject()} [{self.level}] "{self.message}"'
-
 
     @classmethod
     @handle_type_error
