batrick/ceph

mds: track filesystem mirror peers in fsmap
There is a need to track mirror peers for a ceph filesystem
to be used by cephfs-mirror daemons for synchronizing data
(snapshots) to its peers.

One approach would have been to persist peer information
(remote cluster name and user {client-id}) in RADOS, possibly
in omap, and have cephfs-mirror daemons periodically scan for
peer changes. Another approach is to track filesystem mirror
peers in FSMap. This makes cephfs-mirror daemon receive peer
updates and does away with any kind of repetitive scans.
Also, this makes peer information available to FSMap
subscribers to take action based on list of peers.

Signed-off-by: Venky Shankar <vshankar@redhat.com>

src/mon/MonCommands.h
@@ -429,6 +429,20 @@ COMMAND_WITH_FLAG("fs set_default name=fs_name,type=CephString",
 COMMAND("fs set-default name=fs_name,type=CephString",
 	"set the default to the named filesystem",
 	"fs", "rw")
+COMMAND("fs mirror enable "
+	"name=fs_name,type=CephString ",
+	"enable mirroring for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror disable "
+	"name=fs_name,type=CephString ",
+	"disable mirroring for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror peer_add "
+	"name=fs_name,type=CephString "
+	"name=remote_cluster_spec,type=CephString ",
+	"add a mirror peer for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror peer_remove "
+	"name=fs_name,type=CephString "
+	"name=uuid,type=CephString ",
+	"remove a mirror peer for a ceph filesystem", "mds", "rw")
 
 /*
  * Monmap commands

sunnyku/ceph-ci

src/mon/MonCommands.h
@@ -429,6 +429,21 @@ COMMAND_WITH_FLAG("fs set_default name=fs_name,type=CephString",
 COMMAND("fs set-default name=fs_name,type=CephString",
 	"set the default to the named filesystem",
 	"fs", "rw")
+COMMAND("fs mirror enable "
+	"name=fs_name,type=CephString ",
+	"enable mirroring for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror disable "
+	"name=fs_name,type=CephString ",
+	"disable mirroring for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror peer_add "
+	"name=fs_name,type=CephString "
+	"name=remote_cluster_spec,type=CephString "
+	"name=remote_fs_name,type=CephString",
+	"add a mirror peer for a ceph filesystem", "mds", "rw")
+COMMAND("fs mirror peer_remove "
+	"name=fs_name,type=CephString "
+	"name=uuid,type=CephString ",
+	"remove a mirror peer for a ceph filesystem", "mds", "rw")
 
 /*
  * Monmap commands