4c3b7a5f2d0d872541427b633efecb688af1cfdd
Mykola Golub <mgolub@suse.com>
2020-08-22 11:33:45 +0100

    debug TestLibRBD.QuiesceWatchTimeout
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- qa/workunits/rbd/test_librbd.sh
+++ qa/workunits/rbd/test_librbd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
 if [ -n "${VALGRIND}" ]; then
-  valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
-    --error-exitcode=1 ceph_test_librbd
+  CEPH_ARGS=--debug-rbd=30 valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
+    --error-exitcode=1 ceph_test_librbd --gtest_filter=TestLibRBD.QuiesceWatchError --gtest_repeat=100
 else
-  ceph_test_librbd
+  CEPH_ARGS=--debug-rbd=30 ceph_test_librbd --gtest_filter=TestLibRBD.QuiesceWatchError --gtest_repeat=400
 fi
 exit 0
