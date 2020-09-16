ba0105cc65623436138bcc93c4de1b07917831aa
Varsha Rao <varao@redhat.com>
2020-09-16 19:08:51 +0530

    Remove the slash
    Signed-off-by: Varsha Rao <varao@redhat.com>

--- src/pybind/mgr/rook/module.py
+++ src/pybind/mgr/rook/module.py
@@ -82,7 +82,7 @@ class RookEnv(object):
         self.crd_version = os.environ.get('ROOK_CEPH_CLUSTER_CRD_VERSION', 'v1')
         self.api_name = "ceph.rook.io/" + self.crd_version
 
-        self.log.warning(f"THE POD_NAMESPACE = {self.namespace}, cluster_name/ROOK_CEPH_CLUSTER_CRD_NAME = {self.cluster_name}, "
+        self.log.warning(f"THE POD_NAMESPACE = {self.namespace}, cluster_name-ROOK_CEPH_CLUSTER_CRD_NAME = {self.cluster_name}, "
                          f"operator_namespace = {self.operator_namespace}, crd_version = {self.crd_version}, api_name = {self.api_name\n")
 
     def api_version_match(self):
